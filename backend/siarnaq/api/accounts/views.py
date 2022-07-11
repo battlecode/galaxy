b9f4f8ee70d3e316df89a2b94663a050845b4892
Jerry Mao <j-mao@users.noreply.github.com>
2022-07-11 00:17:41 -0400

    Remove nonconformant template boilerplate

--- backend/siarnaq/api/accounts/views.py
+++ backend/siarnaq/api/accounts/views.py
@@ -1,31 +1,3 @@
-from django.contrib.auth import get_user_model
-from rest_framework import mixins, viewsets
-from rest_framework.pagination import PageNumberPagination
+# from django.shortcuts import render
 
-from siarnaq.api.accounts.models import UserProfile
-from siarnaq.api.accounts.serializers import UserProfileSerializer, UserSerializer
-
-
-class UserViewSet(
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    viewsets.GenericViewSet,
-):
-    serializer_class = UserSerializer
-    queryset = get_user_model().objects.all()
-
-
-class UserProfilePagination(PageNumberPagination):
-    page_size = 20
-
-
-class UserProfileViewSet(
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    viewsets.GenericViewSet,
-):
-    serializer_class = UserProfileSerializer
-    queryset = UserProfile.objects.all()
-    pagination_class = UserProfilePagination
+# Create your views here.
