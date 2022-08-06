7dd74da43c0d3580d41063a32e3b0d85357f83d2
n8kim1 <n8kim1@yahoo.com>
2022-08-05 19:41:00 -0600

    Add frontend files -- starter c-p from saturn

---
+++ frontend/src/sidebar.js
@@ -0,0 +1,226 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import Api from "./api";
+import $ from "jquery";
+
+class NLink extends Component {
+  render() {
+    return (
+      <li>
+        <NavLink
+          {...this.props}
+          activeStyle={{ opacity: 1, fontWeight: 800 }}
+        />
+      </li>
+    );
+  }
+}
+
+class SideBar extends Component {
+  constructor() {
+    super();
+    this.state = { on_team: null, logged_in: null, user: {}, league: {} };
+  }
+
+  componentDidMount() {
+    Api.loginCheck((logged_in) => {
+      this.setState({ logged_in });
+      Api.getUserProfile(
+        function (u) {
+          this.setState({ user: u });
+        }.bind(this)
+      );
+    });
+
+    Api.getUserTeam(
+      function (e) {
+        this.setState({ on_team: e !== null });
+        $(document).ready(function () {
+          window.init_right_menu();
+        });
+      }.bind(this)
+    );
+
+    Api.getLeague(
+      function (l) {
+        this.setState({ league: l });
+      }.bind(this)
+    );
+  }
+
+  isSubmissionEnabled() {
+    if (this.state.user.is_staff == true) {
+      return true;
+    }
+    if (this.state.league.game_released == true) {
+      return true;
+    }
+    return false;
+  }
+
+  // for icon options below, see https://themes-pixeden.com/font-demos/7-stroke/
+
+  render() {
+    return (
+      <div className="sidebar" data-color="anomoly">
+        {" "}
+        {/* data-color is defined in light-bootstrap-dashboard.css */}
+        <div className="sidebar-wrapper">
+          <div className="logo">
+            <a href="/home">
+              <img src="../assets/img/logo.png" />
+            </a>
+            <p>Battlecode 2022</p>
+          </div>
+          <ul className="nav nav-pills nav-stacked">
+            <NLink to={`#`} style={{ visibility: "hidden" }}></NLink>
+            <NLink to={`${process.env.PUBLIC_URL}/home`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-home pe-fw" />
+                Home
+              </p>
+            </NLink>
+            <NLink to={`${process.env.PUBLIC_URL}/getting-started`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-sun pe-fw" />
+                Getting Started
+              </p>
+            </NLink>
+            <NLink to={`${process.env.PUBLIC_URL}/resources`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-note2 pe-fw" />
+                Resources
+              </p>
+            </NLink>
+            <NLink to={`${process.env.PUBLIC_URL}/updates`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-bell pe-fw" />
+                Updates
+              </p>
+            </NLink>
+
+            <br />
+
+            <NLink to={`${process.env.PUBLIC_URL}/tournaments`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-medal pe-fw" />
+                Tournaments
+              </p>
+            </NLink>
+            <NLink to={`${process.env.PUBLIC_URL}/rankings`}>
+              <p
+                style={{
+                  fontWeight: "inherit",
+                  textTransform: "none",
+                  fontSize: "inherit",
+                }}
+              >
+                <i className="pe-7s-graph1 pe-fw" />
+                Rankings
+              </p>
+            </NLink>
+            {/*<NLink to={`${process.env.PUBLIC_URL}/search`}><p style={{fontWeight: "inherit", textTransform: "none", fontSize: "inherit"}}><i className="pe-7s-search pe-fw" />Search</p></NLink>*/}
+
+            <br />
+
+            {this.state.logged_in && (
+              <NLink to={`${process.env.PUBLIC_URL}/team`}>
+                <p
+                  style={{
+                    fontWeight: "inherit",
+                    textTransform: "none",
+                    fontSize: "inherit",
+                  }}
+                >
+                  <i className="pe-7s-users pe-fw" />
+                  Team
+                </p>
+              </NLink>
+            )}
+            {this.state.on_team && (
+              <NLink to={`${process.env.PUBLIC_URL}/submissions`}>
+                <p
+                  style={{
+                    fontWeight: "inherit",
+                    textTransform: "none",
+                    fontSize: "inherit",
+                  }}
+                >
+                  <i className="pe-7s-up-arrow pe-fw" />
+                  Submissions
+                </p>
+              </NLink>
+            )}
+            {/*{ this.state.on_team && <NLink to={`${process.env.PUBLIC_URL}/ide`}><p style={{fontWeight: "inherit", textTransform: "none", fontSize: "inherit"}}><i className="pe-7s-pen pe-fw" />IDE</p></NLink> }*/}
+            {this.state.on_team && this.isSubmissionEnabled() && (
+              <NLink to={`${process.env.PUBLIC_URL}/scrimmaging`}>
+                <p
+                  style={{
+                    fontWeight: "inherit",
+                    textTransform: "none",
+                    fontSize: "inherit",
+                  }}
+                >
+                  <i className="pe-7s-joy pe-fw" />
+                  Scrimmaging
+                </p>
+              </NLink>
+            )}
+            {/*<NLink to={`${process.env.PUBLIC_URL}/replay`}><p style={{fontWeight: "inherit", textTransform: "none", fontSize: "inherit"}}><i className="pe-7s-monitor pe-fw" />Replay</p></NLink>*/}
+
+            <br />
+
+            {this.state.user.is_staff && (
+              <NLink to={`${process.env.PUBLIC_URL}/staff`}>
+                <p
+                  style={{
+                    fontWeight: "inherit",
+                    textTransform: "none",
+                    fontSize: "inherit",
+                  }}
+                >
+                  <i className="pe-7s-tools pe-fw" />
+                  Staff
+                </p>
+              </NLink>
+            )}
+
+            <br />
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
---
+++ frontend/src/views/register.js
@@ -0,0 +1,246 @@
+import React, { Component } from "react";
+import Api from "../api";
+
+class Register extends Component {
+  state = {
+    email: "",
+    password: "",
+    username: "",
+    first: "",
+    last: "",
+    dob: "",
+    register: false,
+    error: "",
+    success: "",
+  };
+
+  forgotPassword = () => {
+    window.location.replace("/forgotPassword");
+  };
+
+  callback = (message, success) => {
+    if (success) {
+      window.location.assign("/");
+    } else {
+      this.setState({
+        error: message,
+      });
+    }
+  };
+
+  formSubmit = (e) => {
+    console.log("HI!");
+    e.preventDefault();
+    this.submitRegister();
+  };
+
+  submitLogin = () => {
+    const { username, password } = this.state;
+    Api.login(username, password, this.callback);
+  };
+
+  submitRegister = () => {
+    const { username, register, email, first, last, dob, password } =
+      this.state;
+    // ensure that all fields are correct
+    if (username.length < 4)
+      this.setState({ error: "Username must be at least 4 characters." });
+    else if (email.length < 4)
+      this.setState({ error: "Email must be at least 4 characters." });
+    else if (username.indexOf(".") > -1)
+      this.setState({ error: "Username must not contain dots." });
+    else if (!first) this.setState({ error: "Must provide first name." });
+    else if (!last) this.setState({ error: "Must provide last name." });
+    else if (!dob.match(/^\d{4}-\d{2}-\d{2}$/g))
+      this.setState({ error: "Must provide DOB in YYYY-MM-DD form." });
+    else if (password.length < 6)
+      this.setState({ error: "Password must be at least 6 characters." });
+    else {
+      Api.register(email, username, password, first, last, dob, this.callback);
+    }
+  };
+
+  changeHandler = (e) => {
+    const { id } = e.target;
+    const val = e.target.value;
+    this.setState({ [id]: val });
+  };
+
+  render() {
+    const { error, success, register } = this.state;
+
+    const errorDiv = error && (
+      <div
+        className="card"
+        style={{
+          padding: "20px",
+          width: "350px",
+          margin: "40px auto",
+          marginBottom: "0px",
+          fontSize: "1.1em",
+        }}
+      >
+        <b>Error. </b>
+        {error}
+      </div>
+    );
+
+    const successDiv = success && (
+      <div
+        className="card"
+        style={{
+          padding: "20px",
+          width: "350px",
+          margin: "40px auto",
+          marginBottom: "0px",
+          fontSize: "1.1em",
+        }}
+      >
+        <b>Success.</b> {success}
+      </div>
+    );
+
+    let buttons = (
+      <button
+        type="submit"
+        value="submit"
+        className="btn btn-primary btn-block btn-fill"
+      >
+        Register
+      </button>
+    );
+
+    return (
+      <div
+        className="content"
+        style={{
+          height: "100vh",
+          width: "100vw",
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+        }}
+      >
+        <div
+          className="dustBackground"
+          style={{
+            height: "100vh",
+            width: "100vw",
+            position: "fixed",
+            top: "0px",
+            left: "0px",
+            zIndex: "-1",
+          }}
+        ></div>
+        <h1
+          style={{
+            textAlign: "center",
+            fontWeight: "bold",
+            color: "white",
+          }}
+        >
+          Battlecode 2022
+        </h1>
+        <p
+          style={{
+            textAlign: "center",
+            fontWeight: "bold",
+            color: "white",
+          }}
+        >
+          Register below to participate in Battlecode 2022!
+        </p>
+        {errorDiv}
+        {successDiv}
+        <form onSubmit={this.formSubmit}>
+          <div
+            className="card"
+            style={{
+              width: "350px",
+              margin: error ? "20px auto" : "40px auto",
+            }}
+          >
+            <div className="content">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <label>Username</label>
+                    <input
+                      type="text"
+                      id="username"
+                      className="form-control"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                  <div class="clearfix"></div>
+                </div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label>First Name</label>
+                    <input
+                      type="text"
+                      id="first"
+                      className="form-control"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label>Last Name</label>
+                    <input
+                      type="text"
+                      id="last"
+                      className="form-control"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      id="email"
+                      className="form-control"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+                <div className="col-xs-6">
+                  <div className="form-group">
+                    <label>Date of Birth</label>
+                    <input
+                      type="text"
+                      id="dob"
+                      placeholder="YYYY-MM-DD"
+                      className="form-control"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+                <div class="clearfix"></div>
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <label>Password</label>
+                    <input
+                      type="password"
+                      id="password"
+                      className="form-control"
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+              </div>
+              {buttons}
+
+              <div className="clearfix" />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
