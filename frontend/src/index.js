9d5d444190635b90d7350ce0e0f6bc0a8108b9b6
n8kim1 <n8kim1@yahoo.com>
2022-08-05 19:41:00 -0600

    Add frontend files -- starter c-p from saturn

---
+++ frontend/src/index.js
@@ -0,0 +1,243 @@
+import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router";
+
+import Home from "./views/home";
+import NotFound from "./views/not_found";
+import GettingStarted from "./views/getting-started";
+import Scrimmaging from "./views/scrimmaging";
+import Tournaments from "./views/tournaments";
+import Updates from "./views/updates";
+import Search from "./views/search";
+import Team from "./views/team";
+import Issues from "./views/issues";
+import Debugging from "./views/debugging";
+import CodeOfConduct from "./views/codeofconduct";
+import Staff from "./views/staff";
+import Rankings from "./views/rankings";
+import Account from "./views/account";
+import Resources from "./views/resources";
+import LoginRegister from "./views/login";
+import Register from "./views/register";
+// commented but kept since we might use this later
+// import VerifyUser from "./views/VerifyUser";
+import PasswordForgot from "./views/passwordForgot";
+import PasswordChange from "./views/passwordChange";
+import Submissions from "./views/submissions";
+import TeamInfo from "./views/teamInfo";
+import Footer from "./footer";
+import NavBar from "./navbar";
+import SideBar from "./sidebar";
+import Api from "./api";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { logged_in: null, user: {}, league: {} };
+  }
+
+  componentDidMount() {
+    Api.loginCheck((logged_in) => {
+      this.setState({ logged_in });
+
+      Api.getUserProfile(
+        function (u) {
+          this.setState({ user: u });
+        }.bind(this)
+      );
+
+      Api.getLeague(
+        function (l) {
+          console.log(l);
+          this.setState({ league: l });
+        }.bind(this)
+      );
+    });
+  }
+
+  isSubmissionEnabled() {
+    if (this.state.user.is_staff === true) {
+      return true;
+    }
+    if (this.state.league.game_released === true) {
+      return true;
+    }
+    return false;
+  }
+
+  userIsStaff() {
+    return this.state.user.is_staff === true;
+  }
+
+  render() {
+    // direct to home page, should always be visible
+    let homeElems = [
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />,
+      <Route path={`${process.env.PUBLIC_URL}/home`} component={Home} />,
+    ];
+
+    // should only be visible to logged in users
+    let loggedInElems = [];
+    if (this.state.logged_in) {
+      loggedInElems = [
+        <Route path={`${process.env.PUBLIC_URL}/team`} component={Team} />,
+        <Route
+          path={`${process.env.PUBLIC_URL}/account`}
+          component={Account}
+        />,
+        <Route
+          path={`${process.env.PUBLIC_URL}/password_forgot`}
+          component={Home}
+        />,
+        <Route
+          path={`${process.env.PUBLIC_URL}/password_change`}
+          component={Home}
+        />,
+        <Route path={`${process.env.PUBLIC_URL}/login`} component={Home} />,
+        <Route
+          path={`${process.env.PUBLIC_URL}/scrimmaging`}
+          component={Scrimmaging}
+        />,
+        <Route
+          path={`${process.env.PUBLIC_URL}/submissions`}
+          component={Submissions}
+        />,
+        <Route path={`${process.env.PUBLIC_URL}/register`} component={Home} />,
+      ];
+    }
+
+    // should be visible to all users
+    let nonLoggedInElems = [
+      <Route path={`${process.env.PUBLIC_URL}/updates`} component={Updates} />,
+      <Route path={`${process.env.PUBLIC_URL}/search`} component={Search} />,
+      <Route
+        path={`${process.env.PUBLIC_URL}/tournaments`}
+        component={Tournaments}
+      />,
+      <Route
+        path={`${process.env.PUBLIC_URL}/:year/getting-started`}
+        component={GettingStarted}
+      />,
+      <Route
+        path={`${process.env.PUBLIC_URL}/common-issues`}
+        component={Issues}
+      />,
+      <Route
+        path={`${process.env.PUBLIC_URL}/debugging`}
+        component={Debugging}
+      />,
+      <Route
+        path={`${process.env.PUBLIC_URL}/codeofconduct`}
+        component={CodeOfConduct}
+      />,
+      <Route
+        path={`${process.env.PUBLIC_URL}/resources`}
+        component={Resources}
+      />,
+      <Route
+        path={`${process.env.PUBLIC_URL}/rankings/:team_id`}
+        component={TeamInfo}
+      />,
+      <Route
+        path={`${process.env.PUBLIC_URL}/rankings`}
+        component={Rankings}
+      />,
+      <Route path="*" component={NotFound} />,
+    ];
+
+    let staffElems = [];
+    if (this.userIsStaff()) {
+      staffElems = [
+        <Route path={`${process.env.PUBLIC_URL}/staff`} component={Staff} />,
+      ];
+    }
+
+    // should only be visible if user is staff or submissions are enabled
+    // let gameElems = []
+    // if (this.isSubmissionEnabled()) {
+    //   gameElems = [
+    //     <Route path={`${process.env.PUBLIC_URL}/scrimmaging`} component={Scrimmaging} />,
+    //     <Route path={`${process.env.PUBLIC_URL}/submissions`} component={Submissions} />
+    //   ]
+    // }
+
+    return (
+      <div className="wrapper">
+        <SideBar />
+        <div className="main-panel">
+          <NavBar />
+          <Switch>
+            {homeElems}
+            {/* { gameElems } */}
+            {staffElems}
+            {loggedInElems}
+            {nonLoggedInElems}
+          </Switch>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+}
+
+class BeforeLoginApp extends Component {
+  constructor() {
+    super();
+    this.state = { logged_in: null };
+  }
+
+  componentDidMount() {
+    Api.loginCheck((logged_in) => {
+      this.setState({ logged_in });
+    });
+  }
+
+  render() {
+    if (this.state.logged_in) {
+      return <App />;
+    }
+    if (this.state.logged_in === false) {
+      return (
+        <Switch>
+          <Route
+            path={`${process.env.PUBLIC_URL}/password_forgot`}
+            component={PasswordForgot}
+          />
+          <Route
+            path={`${process.env.PUBLIC_URL}/password_change`}
+            component={PasswordChange}
+          />
+          <Route
+            path={`${process.env.PUBLIC_URL}/login`}
+            component={LoginRegister}
+          />
+          <Route
+            path={`${process.env.PUBLIC_URL}/register`}
+            component={Register}
+          />
+          <Route
+            path={`${process.env.PUBLIC_URL}/team`}
+            component={LoginRegister}
+          />
+          ,
+          <Route
+            path={`${process.env.PUBLIC_URL}/account`}
+            component={LoginRegister}
+          />
+          <Route path="*" component={App} />
+        </Switch>
+      );
+    }
+    return <div />;
+  }
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <BrowserRouter>
+    <BeforeLoginApp />
+  </BrowserRouter>
+);
---
+++ frontend/src/navbar.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import Api from "./api";
+
+class NavBar extends Component {
+  toggleNavigation() {
+    window.click_toggle();
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button
+              type="button"
+              onClick={this.toggleNavigation}
+              className="navbar-toggle"
+              data-toggle="collapse"
+              data-target="#navigation-example-2"
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+            </button>
+            <NavLink
+              className="navbar-brand"
+              to={`${process.env.PUBLIC_URL}/home`}
+            >
+              Battlecode 2022
+            </NavLink>
+          </div>
+          <div className="collapse navbar-collapse">
+            <NavBarAccount />
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+class NavBarAccount extends Component {
+  constructor() {
+    super();
+    this.state = { logged_in: null };
+  }
+  componentDidMount() {
+    Api.loginCheck((logged_in) => {
+      this.setState({ logged_in });
+    });
+  }
+  logout() {
+    Api.logout(function (e) {
+      window.location.reload();
+    });
+  }
+  render() {
+    if (this.state.logged_in) {
+      return (
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <NavLink to={`${process.env.PUBLIC_URL}/account`}>Account</NavLink>
+          </li>
+          <li>
+            <a onClick={this.logout}>Log out</a>
+          </li>
+        </ul>
+      );
+    }
+    if (this.state.logged_in === false) {
+      return (
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <NavLink to={`${process.env.PUBLIC_URL}/register`}>
+              Register
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={`${process.env.PUBLIC_URL}/login`}>Log in</NavLink>
+          </li>
+        </ul>
+      );
+    }
+    return <ul />;
+  }
+}
+
+export default NavBar;
