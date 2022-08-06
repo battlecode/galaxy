749183ef55827511aa6e29333ed176f6d06a4cdb
n8kim1 <n8kim1@yahoo.com>
2022-08-05 19:41:00 -0600

    Add frontend files -- starter c-p from saturn

---
+++ frontend/src/views/rankings.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import Api from "../api";
+
+import RankingTeamList from "../components/rankingTeamList";
+
+class Rankings extends Component {
+  state = {
+    teams: null,
+    teamLimit: 0,
+    teamPage: 1,
+    input: "",
+  };
+
+  componentDidMount() {
+    const { input } = this.state;
+    Api.searchTeamRanking(input, 1, this.onDataLoad);
+  }
+
+  handleChange = (e) => {
+    const { input } = this.state;
+    this.setState({ input: e.target.value });
+  };
+
+  onDataLoad = (data) => {
+    //this.setState({teams: [{id: "hi", mu: 5, name: "Example Team", users: ["User #1", "User #2", "User #3"], bio: "dfghjkiuytregfdawsqA DKFODLKLASDKA sdadasdas"}]})
+    this.setState(data);
+  };
+
+  getTeamPage = (page) => {
+    const { state } = this;
+    if (page !== state.teamPage && page >= 0 && page <= state.teamLimit) {
+      Api.searchTeamRanking(state.input, page, this.onDataLoad);
+    }
+  };
+
+  search = (e) => {
+    const { input } = this.state;
+    e.preventDefault();
+    Api.searchTeamRanking(input, 1, this.onDataLoad);
+  };
+
+  render() {
+    const { state } = this;
+    return (
+      <div className="content">
+        <div className="container-fluid row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="content">
+                <form className="input-group" onSubmit={this.search}>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={this.handleChange}
+                    placeholder="Search for a Team or User..."
+                  />
+                  <span className="input-group-btn">
+                    <button
+                      className="btn btn-default"
+                      type="submit"
+                      value="Submit"
+                    >
+                      Go!
+                    </button>
+                  </span>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-12">
+            <RankingTeamList
+              teams={state.teams}
+              page={state.teamPage}
+              pageLimit={state.teamLimit}
+              onPageClick={this.getTeamPage}
+              history={this.props.history}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Rankings;
