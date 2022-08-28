1e4501c645acec89bc37308290123ede208d4cf8
Gaurav Arya <gauravarya272@gmail.com>
2022-08-28 09:22:57 -0400

    Move simplejwt token API to top level

--- backend/siarnaq/api/accounts/urls.py
+++ backend/siarnaq/api/accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from siarnaq.api.accounts import views
 
@@ -8,7 +7,5 @@ router = DefaultRouter()
 router.register("profile", views.UserProfileViewSet, basename="profile")
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
 ]
