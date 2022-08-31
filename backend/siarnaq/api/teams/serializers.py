c685b69f02afdf7bd52643fe4476d0203a3366e3
Serena Li <serena.li.usa@gmail.com>
2022-08-31 16:32:40 -0500

    commit to show teamwriteserializer

--- backend/siarnaq/api/teams/serializers.py
+++ backend/siarnaq/api/teams/serializers.py
@@ -1,39 +1,100 @@
 from rest_framework import serializers
-from siarnaq.api.teams.models import Team, TeamProfile, TeamStatus
+from siarnaq.api.teams.models import Team, TeamProfile, TeamStatus, EligibilityCriterion, Rating
 from django.contrib.auth import get_user_model
 
-class TeamProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TeamProfile
-        fields = ['quote']#'__all__'
+# class BasicRatingSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Rating 
+#         fields = [
+#             'mean',
+#             'updated'
+#         ]
+
+# class TeamProfileSerializer(serializers.ModelSerializer):
+#     rating = BasicRatingSerializer(required=True)
+#     class Meta:
+#         model = TeamProfile
+#         fields = [
+#             "quote",
+#             "biography",
+#             "has_avatar",
+#             "rating",
+#             "auto_accept_ranked",
+#             "auto_accept_unranked",
+#             "eligible_for",
+#         ]
+#         read_only_fields = ["rating"]
+
 
 # https://stackoverflow.com/a/45632937
-# https://www.django-rest-framework.org/api-guide/relations/#writable-nested-serializers 
+# https://www.django-rest-framework.org/api-guide/relations/#writable-nested-serializers
 # https://stackoverflow.com/questions/6996176/how-to-create-an-object-for-a-django-model-with-a-many-to-many-field
-class TeamSerializer(serializers.ModelSerializer):
-    team_profile = TeamProfileSerializer(required=True)
-    members = serializers.SlugRelatedField(
-        queryset=get_user_model().objects.all(), slug_field='username', many=True)
-    
+# https://www.django-rest-framework.org/api-guide/serializers/#writable-nested-representations
+# https://www.cdrf.co/3.1/rest_framework.mixins/UpdateModelMixin.html 
+# https://www.cdrf.co/3.1/rest_framework.serializers/ModelSerializer.html
+# https://www.django-rest-framework.org/api-guide/serializers/#serializer-inheritance
+
+# class FullTeamSerializer(serializers.ModelSerializer):
+#     profile = TeamProfileSerializer(required=True)
+#     class Meta:
+#         model = Team
+#         fields = ["episode", "name", "members", "join_key", "status", "profile"]
+#         read_only_fields = ["join_key"]
+
+#     def create(self, validated_data):
+#         profile_data = validated_data.pop("profile")
+#         members_data = validated_data.pop("members")
+#         team_obj = Team.objects.create(**validated_data)
+#         team_obj.members.add(*members_data)
+#         team_obj.save()
+#         # print("members_data", *members_data)
+#         # team.members.add(*members_data)
+#         eligible_for_data = profile_data.pop("eligible_for")
+#         rating_obj = Rating.objects.create()
+#         profile_obj = TeamProfile.objects.create(team=team_obj, rating=rating_obj, **profile_data)
+#         profile_obj.eligible_for.add(*eligible_for_data)
+#         profile_obj.save()
+#         return team_obj
+
+#     def update(self, instance, validated_data):
+#         pass
+
+
+class TeamProfileWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TeamProfile
+        fields = [
+            "quote",
+            "biography",
+            "has_avatar",
+            "auto_accept_ranked",
+            "auto_accept_unranked",
+            "eligible_for",
+        ]
+
+class TeamWriteSerializer(serializers.ModelSerializer):
+    profile = TeamProfileWriteSerializer(required=True)
     class Meta:
         model = Team
-        fields = ['episode', 'name', 'status', 'team_profile', 'members']
-
-    # def is_valid(self):
-    #     super().is_valid()
-
+        fields = ["episode", "name", "members", "status", "profile"]
 
     def create(self, validated_data):
-        print('vvalidated_data', validated_data)
-        #members_data = validated_data.pop('team_members')
-        profile_data = validated_data.pop('team_profile')
-        members_data = validated_data.pop('members')
-        team = Team.objects.create(**validated_data)
-        print('members_data', *members_data)
-        team.members.add(*members_data)
-        team.save()
-        print('ppprofile_data', profile_data)
-        TeamProfile.objects.create(team=team.id, **profile_data)
-        return team
+        profile_data = validated_data.pop("profile")
+        members_data = validated_data.pop("members")
+        team_obj = Team.objects.create(**validated_data)
+        # add members separately bc we can't initialize a ManyToManyField on obj creation
+        team_obj.members.add(*members_data)
+        team_obj.save()
+
+        eligible_for_data = profile_data.pop("eligible_for")
+        # set rating to default rating
+        rating_obj = Rating.objects.create()
+        profile_obj = TeamProfile.objects.create(team=team_obj, rating=rating_obj, **profile_data)
+        # add eligibility separately bc it's a ManyToManyField
+        profile_obj.eligible_for.add(*eligible_for_data)
+        profile_obj.save()
+        return team_obj
+    
     def update(self, instance, validated_data):
+        # TODO implement this
         pass
--- backend/siarnaq/api/teams/signals.py
+++ backend/siarnaq/api/teams/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import pre_save
-from django.db.models import Base import django.db.models.BaseUserManager.make_random_password
+from django.dispatch import receiver
+from siarnaq.api.teams.models import Team
+from django.utils.crypto import get_random_string
 
 @receiver(pre_save, sender=Team)
 def gen_team_key(sender, instance, raw, update_fields, **kwargs):
@@ -7,4 +9,5 @@ def gen_team_key(sender, instance, raw, update_fields, **kwargs):
     Generate a new team key.
     """
     if not raw and instance._state.adding:
-        instance.join_key = uuid.uuid4().hex[:16]
+        print('setting new join_key')
+        instance.join_key = get_random_string(24)
