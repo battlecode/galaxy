362417b041ec9b1553332385421093d7dd3cf523
n8kim1 <n8kim1@yahoo.com>
2022-09-01 13:24:43 -0400

    Use countdown package for splitting time units

--- frontend/src/components/countdown.js
+++ frontend/src/components/countdown.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Api from "../api";
+import Duration from "duration";
 
 class Countdown extends Component {
   constructor() {
@@ -82,40 +83,19 @@ class Countdown extends Component {
   // If endDate is in the future: returns a dict of {years:, ..., sec:} representing the time left
   // If not: returns false
   calculateCountdown(endDate) {
-    let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
+    const submission_countdown = new Duration(new Date(), endDate);
 
     // clear countdown when date is reached
-    if (diff <= 0) return false;
+    if (submission_countdown.valueOf() <= 0) return false;
 
     const timeLeft = {
-      years: 0,
-      days: 0,
-      hours: 0,
-      min: 0,
-      sec: 0,
+      // Be careful of using full units vs trailing units
+      // See https://www.npmjs.com/package/duration
+      days: submission_countdown.days,
+      hours: submission_countdown.hour,
+      min: submission_countdown.minute,
+      sec: submission_countdown.second,
     };
-
-    // calculate time difference between now and expected date
-    if (diff >= 365.25 * 86400) {
-      // 365.25 * 24 * 60 * 60
-      timeLeft.years = Math.floor(diff / (365.25 * 86400));
-      diff -= timeLeft.years * 365.25 * 86400;
-    }
-    if (diff >= 86400) {
-      // 24 * 60 * 60
-      timeLeft.days = Math.floor(diff / 86400);
-      diff -= timeLeft.days * 86400;
-    }
-    if (diff >= 3600) {
-      // 60 * 60
-      timeLeft.hours = Math.floor(diff / 3600);
-      diff -= timeLeft.hours * 3600;
-    }
-    if (diff >= 60) {
-      timeLeft.min = Math.floor(diff / 60);
-      diff -= timeLeft.min * 60;
-    }
-    timeLeft.sec = diff;
 
     return timeLeft;
   }
