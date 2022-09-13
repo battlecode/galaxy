f37957d1a4532f26a7dbfeb1420f55202671c938
n8kim1 <n8kim1@yahoo.com>
2022-09-13 14:16:24 -0400

    Move some (unused) css to css file

--- frontend/src/views/issues.js
+++ frontend/src/views/issues.js
@@ -2,16 +2,6 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
-const He = styled.h5`
-  font-weight: bold;
-  font-size: 1.3em;
-`;
-
-const Hee = styled.h5`
-  text-decoration: underline;
-  font-size: 1.2em;
-`;
-
 class Issues extends Component {
   render() {
     return (
