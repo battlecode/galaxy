300f9c46ec2a9f7b015e463587cb6ad660ae95f8
Jerry Mao <j-mao@users.noreply.github.com>
2022-10-31 23:08:58 -0400

    Paginate long responses

--- backend/siarnaq/settings.py
+++ backend/siarnaq/settings.py
@@ -134,6 +134,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 25,
 }
 
 # Templates
