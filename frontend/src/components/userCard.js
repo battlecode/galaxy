3dc0f2e401306316cb7f642e188d260de9140a09
Gaurav Arya <gauravarya272@gmail.com>
2022-11-06 20:59:40 -0500

    More changes to adapt to new user schema

--- frontend/src/components/userCard.js
+++ frontend/src/components/userCard.js
@@ -3,27 +3,29 @@ import Avatar from "../components/avatar";
 
 class UserCard extends Component {
   render() {
-    const user = this.props.user;
-    const staff_msg = user.is_staff ? (
-      <small>
-        {" "}
-        | <label>Staff</label>
-      </small>
-    ) : null;
+    const user_profile = this.props.user_profile;
+    // const staff_msg = user.is_staff ? (
+    //   <small>
+    //     {" "}
+    //     | <label>Staff</label>
+    //   </small>
+    // ) : null;
+    const staff_msg = null;
+    // TODO: staff message?
     return (
       <div className="card card-user">
         <div className="image"></div>
         <div className="content">
           <div className="author">
-            <Avatar data={user} />
+            {/*<Avatar data={user} />*/}
             <h4 className="title">
-              {user.first_name + " " + user.last_name}
+              {user_profile.user.first_name + " " + user_profile.user.last_name}
               <br />
-              <small>{user.username}</small> {staff_msg}{" "}
+              <small>{user_profile.user.username}</small> {staff_msg}{" "}
             </h4>
           </div>
           <br />
-          <p className="description text-center">{user.bio}</p>
+          <p className="description text-center">{user_profile.biography}</p>
         </div>
       </div>
     );
