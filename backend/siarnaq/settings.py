b9328f43910b4e50225f445154da89ed5ee1981e
Gaurav Arya <gauravarya272@gmail.com>
2022-11-06 23:32:22 -0500

    Place CORS middleware first

--- backend/siarnaq/settings.py
+++ backend/siarnaq/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # Place CORS first. See https://stackoverflow.com/a/45376281.
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -64,7 +66,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
