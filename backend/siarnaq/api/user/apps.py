eb2292bba49c08c72b65cbf511cb3a20d45f1ac1
n8kim1 <n8kim1@yahoo.com>
2022-11-12 16:44:25 -0500

    Register signal for user email

--- backend/siarnaq/api/user/apps.py
+++ backend/siarnaq/api/user/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "siarnaq.api.user"
+
+    def ready(self):
+        from . import signals  # noqa: F401
