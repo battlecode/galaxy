8869ccd5849e483777e7c736a496f2798495f6a2
Gaurav Arya <gauravarya272@gmail.com>
2022-11-13 16:09:35 -0500

    Prevent joining an inactive team

--- backend/siarnaq/api/teams/signals.py
+++ backend/siarnaq/api/teams/signals.py
@@ -44,3 +44,9 @@ def prevent_team_exceed_capacity(instance, action, **kwargs):
     if action == "pre_add":
         if instance.members.count() == settings.MAX_TEAM_SIZE:
             raise ValidationError("Maximum number of team members exceeded.")
+
+
+@receiver(m2m_changed, sender=Team.members.through)
+def prevent_joining_inactive_team(instance, action, **kwargs):
+    if action == "pre_add" and instance.status == TeamStatus.INACTIVE:
+        raise ValidationError("Cannot join an inactive team.")
