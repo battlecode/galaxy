65365753f03009ffd2d1924b26cb382447fcc491
n8kim1 <n8kim1@yahoo.com>
2022-11-14 09:53:09 -0500

    Add token verify to backend

--- backend/siarnaq/urls.py
+++ backend/siarnaq/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,6 +30,7 @@ urlpatterns = [
     path("api/team/", include("siarnaq.api.teams.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     # Schema:
     path("api/specs/", SpectacularAPIView.as_view(), name="specs"),
     path(
