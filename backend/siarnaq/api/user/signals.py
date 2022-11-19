76bc79f094588cc3f170976ec07505067052914b
n8kim1 <n8kim1@yahoo.com>
2022-11-19 14:28:24 -0500

    Use real data in emails

--- backend/siarnaq/api/user/signals.py
+++ backend/siarnaq/api/user/signals.py
@@ -14,7 +14,6 @@ def password_reset_token_created(
     When a token is created, send an email to the user.
     See https://github.com/anexia-it/django-rest-passwordreset/blob/master/README.md.
     """
-    print("Sending email...")
     user_email = reset_password_token.user.email
     context = {
         "username": reset_password_token.user.username,
@@ -25,18 +24,15 @@ def password_reset_token_created(
             reset_password_token.key,
         ),
     }
-    # email_html_message = render_to_string(
-    #     "siarnaq/templates/password_reset.html", context)
-    # print(user_email)
+    email_html_message = render_to_string("../templates/password_reset.html", context)
 
     send_mail(
         # title:
         "Battlecode Password Reset Token",
         # message:
-        "Hi Jerry! You have requested a password reset. Unfortunately we have no template yet...",
-        # email_html_message,
+        email_html_message,
         # from:
         settings.EMAIL_HOST_USER,
         # to:
-        ["n8kim1@gmail.com"],
+        [user_email],
     )
