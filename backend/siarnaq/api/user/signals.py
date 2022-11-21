4e646193b7ae9067c2230e430b3afcc77c4afba6
n8kim1 <n8kim1@yahoo.com>
2022-11-20 20:08:47 -0500

    Only send emails if enabled

--- backend/siarnaq/api/user/signals.py
+++ backend/siarnaq/api/user/signals.py
@@ -14,25 +14,26 @@ def password_reset_token_created(
     When a token is created, send an email to the user.
     See https://github.com/anexia-it/django-rest-passwordreset/blob/master/README.md.
     """
-    user_email = reset_password_token.user.email
-    context = {
-        "username": reset_password_token.user.username,
-        "reset_password_url": "{}?token={}".format(
-            instance.request.build_absolute_uri(
-                reverse("password_reset:reset-password-confirm")
+    if settings.EMAIL_ENABLED:
+        user_email = reset_password_token.user.email
+        context = {
+            "username": reset_password_token.user.username,
+            "reset_password_url": "{}?token={}".format(
+                instance.request.build_absolute_uri(
+                    reverse("password_reset:reset-password-confirm")
+                ),
+                reset_password_token.key,
             ),
-            reset_password_token.key,
-        ),
-    }
-    email_html_message = render_to_string("../templates/password_reset.html", context)
-
-    send_mail(
-        # title:
-        "Battlecode Password Reset Token",
-        # message:
-        email_html_message,
-        # from:
-        settings.EMAIL_HOST_USER,
-        # to:
-        [user_email],
-    )
+        }
+        email_html_message = render_to_string(
+            "../templates/password_reset.html", context
+        )
+        send_mail(
+            subject="Battlecode Password Reset Token",
+            msg=email_html_message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user_email],
+            fail_silently=False,
+        )
+    else:
+        raise Exception
