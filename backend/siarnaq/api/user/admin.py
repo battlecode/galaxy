ea1d6a55d9c905d7edec0279c1976122ea8bfe04
Jerry Mao <j-mao@users.noreply.github.com>
2022-11-22 22:12:11 -0500

    Keep user model safe from admins

--- backend/siarnaq/api/user/admin.py
+++ backend/siarnaq/api/user/admin.py
@@ -57,3 +57,29 @@ class UserAdmin(admin.ModelAdmin):
     readonly_fields = ("last_login", "date_joined")
     search_fields = ("username", "first_name", "last_name", "email")
     search_help_text = "Search for a username, name or email."
+
+    def get_form(self, request, obj=None, **kwargs):
+        """Disable form fields that grant too much power."""
+        # See: https://realpython.com/manage-users-in-django-admin/
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields: set[str] = set()
+
+        if not is_superuser:
+            # Non-superusers cannot grant themselves superuser access.
+            # Disable user_permissions to configure entirely via groups.
+            disabled_fields |= {"is_superuser", "user_permissions"}
+
+        if not is_superuser and obj is not None and obj == request.user:
+            # Users cannot change their own access.
+            disabled_fields |= {
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+        return form
