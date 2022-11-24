e7b7b84978bc4f94c5d547eb6b5c116b78342351
Jerry Mao <j-mao@users.noreply.github.com>
2022-11-23 19:15:59 -0500

    Fix incorrect basename

--- backend/siarnaq/api/teams/urls.py
+++ backend/siarnaq/api/teams/urls.py
@@ -4,6 +4,6 @@ from rest_framework.routers import DefaultRouter
 from siarnaq.api.teams import views
 
 router = DefaultRouter()
-router.register(r"(?P<episode_id>.+)/t", views.TeamViewSet, basename="teams-detail")
+router.register(r"(?P<episode_id>.+)/t", views.TeamViewSet, basename="team")
 
 urlpatterns = [path("", include(router.urls))]
