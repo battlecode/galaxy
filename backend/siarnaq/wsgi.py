8a932f3de3f15c70e04a83833402867495440b08
Jerry Mao <j-mao@users.noreply.github.com>
2022-11-26 16:01:55 -0500

    Fix configurations initialization

--- backend/siarnaq/wsgi.py
+++ backend/siarnaq/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
-from configurations.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siarnaq.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
+
+from configurations.wsgi import get_wsgi_application  # noqa: E402
 
 application = get_wsgi_application()
