98ac08a5b518c9ec602f3db1376ae6788f640408
Gaurav Arya <gauravarya272@gmail.com>
2022-11-25 21:33:47 -0500

    Add forgotten spinner file

---
+++ frontend/src/components/spinner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Spinner() {
+  return (
+    <div className="content">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="spinner"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
