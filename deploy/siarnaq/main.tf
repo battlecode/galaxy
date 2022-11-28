5ead79f8b18c12994054c4b3f0c9358aa6b788bc
Jerry Mao <j-mao@users.noreply.github.com>
2022-11-27 22:11:51 -0500

    Configure Cloud Run ingress options

--- deploy/siarnaq/main.tf
+++ deploy/siarnaq/main.tf
@@ -159,6 +159,14 @@ resource "google_cloud_run_service" "this" {
     }
   }
 
+  metadata {
+    annotations = {
+      "run.googleapis.com/ingress" = "internal-and-cloud-load-balancing"
+    }
+  }
+
+  autogenerate_revision_name = true
+
   traffic {
     percent         = 100
     latest_revision = true
