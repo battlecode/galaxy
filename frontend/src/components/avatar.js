e55fcbcf49122551ec97b75a3db46ed455e5b2e9
Nathan Kim <n8kim1@gmail.com>
2022-11-29 14:55:37 -0500

    Clean frontend avi generation

--- frontend/src/components/avatar.js
+++ frontend/src/components/avatar.js
@@ -1,27 +1,13 @@
 import React, { Component } from "react";
 
 /* a component for displaying a user or teams avatar (used on team and user pages)
- * props: data — either user or team, used to get avatar or seed for random generation.
- *				 if data does not have either name or username defined, empty avatar will be returned */
+ * props: data — either user or team, used to get avatar
+ * Assumes that the backend will always return a URL, which Jerry claims to trust */
 class Avatar extends Component {
   render() {
     const data = this.props.data;
-    const has_avatar = data.profile.has_avatar;
     const avatar = data.profile.avatar_url;
-    if (has_avatar) {
-      // avatar is uploaded
-      return <img className="avatar border-gray" src={avatar} alt="Avatar" />;
-    } else {
-      if (!data.name && !data.username && !data.id) {
-        // data not fully loaded, return placeholder
-        return (
-          <div
-            className="avatar border-gray"
-            style={{ display: "inline-block" }}
-          ></div>
-        );
-      }
-    }
+    return <img className="avatar border-gray" src={avatar} alt="Avatar" />;
   }
 }
 
