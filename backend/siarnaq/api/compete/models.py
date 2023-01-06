b1c3ad9a951626fc9034322aa8c3349f778d15ca
Jerry Mao <j-mao@users.noreply.github.com>
2023-01-06 23:24:22 +1100

    Default to http for the sake of staging

--- backend/siarnaq/api/compete/models.py
+++ backend/siarnaq/api/compete/models.py
@@ -163,7 +163,7 @@ class Submission(SaturnInvocation):
 
     def enqueue_options(self):
         """Return the options to be submitted to the compilation queue."""
-        report_url = "https://{}{}".format(
+        report_url = "http://{}{}".format(
             settings.ALLOWED_HOSTS[0],
             reverse(
                 "submission-report",
@@ -256,7 +256,7 @@ class Match(SaturnInvocation):
 
     def enqueue_options(self):
         """Return the options to be submitted to the execution queue."""
-        report_url = "https://{}{}".format(
+        report_url = "http://{}{}".format(
             settings.ALLOWED_HOSTS[0],
             reverse(
                 "match-report",
