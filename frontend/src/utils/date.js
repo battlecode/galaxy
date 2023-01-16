4f84582c3829d9e61674fb7da014cc6ea86a5fbf
Jerry Mao <j-mao@users.noreply.github.com>
2023-01-16 17:39:42 -0500

    Hide seconds from time

--- frontend/src/utils/date.js
+++ frontend/src/utils/date.js
@@ -1,21 +1,16 @@
 function getDateTimeText(date) {
   // Use US localization for standardization in date format
-  const est_string = date.toLocaleString("en-US", {
+  const est_date_str = date.toLocaleString("en-US", {
     timeZone: "EST",
+    timeStyle: "short",
+    dateStyle: "full",
   });
-  // need to pass weekday here, since weekday isn't shown by default
-  const est_day_of_week = date.toLocaleString("en-US", {
-    timeZone: "EST",
-    weekday: "short",
-  });
-  const est_date_str = `${est_day_of_week}, ${est_string}`;
 
   // Allow for localization here
-  const locale_string = date.toLocaleString();
-  const locale_day_of_week = date.toLocaleString([], {
-    weekday: "short",
+  const local_date_str = date.toLocaleString([], {
+    timeStyle: "short",
+    dateStyle: "full",
   });
-  const local_date_str = `${locale_day_of_week}, ${locale_string}`;
 
   // See https://stackoverflow.com/questions/1954397/detect-timezone-abbreviation-using-javascript
   const local_timezone = date
