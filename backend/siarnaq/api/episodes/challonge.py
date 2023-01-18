5e791148e0a018074db7d2d3e3d4f1d51af82070
Nathan Kim <n8kim1@gmail.com>
2023-01-17 19:44:28 -0500

    Refactor queueing match to module; clean some names, imports, etc

--- backend/siarnaq/api/episodes/challonge.py
+++ backend/siarnaq/api/episodes/challonge.py
@@ -4,9 +4,8 @@
 # to improve dev efficiency
 
 import requests
+from django.apps import apps
 from django.conf import settings
-
-from siarnaq.api.compete.models import Match
 
 _headers = {
     "Accept": "application/json",
@@ -89,7 +88,7 @@ def start_tournament(tournament_url):
     r.raise_for_status()
 
 
-def get_tournament(tournament_url):
+def get_tournament_data(tournament_url):
     url = f"{URL_BASE}tournaments/{tournament_url}.json"
 
     r = requests.get(url, headers=_headers)
@@ -98,7 +97,7 @@ def get_tournament(tournament_url):
 
 
 def get_round_indexes(tournament_url):
-    tournament_data = get_tournament(tournament_url)
+    tournament_data = get_tournament_data(tournament_url)
 
     round_indexes = set()
     for item in tournament_data["included"]:
@@ -110,7 +109,101 @@ def get_round_indexes(tournament_url):
     return round_indexes
 
 
-def update_match(tournament_url, match_id, match: Match):
+def get_match_and_participant_objects_for_round(tournament_url, round):
+    tournament_data = get_tournament_data(tournament_url)
+    # Derive matches of this round
+    # NOTE this probably makes more sense (efficiency and consistency)
+    # as a dict. Track in #549
+    matches = []
+    # Takes some wrangling with API response format
+    # We should move this block later
+    # (to keep all code that directly hits challonge
+    # in its own module) Track in #549
+    for item in tournament_data["included"]:
+        # Much cleaner w match-case and multiple keys.
+        # Track in #549
+        if item["type"] == "match":
+            round_idx = item["attributes"]["round"]
+            if round_idx == round.challonge_id:
+                # Only enqueue the round if all matches are "open".
+                # NOTE: it would be good to have a "force re-enqueue round",
+                # which re-enqueues matches even if matches or round
+                # already in progress.
+                # This would change the following check --
+                # matches could be open _or done_.
+                # !!! This is also _really hard_ right now
+                # cuz it involves match deletion which is really hard.
+                # Track in #549
+                if item["attributes"]["state"] != "open":
+                    # For later, have this raise a more specific exception.
+                    # Then have the caller handle this return
+                    # and translate it into an HTTP response.
+                    raise RuntimeError(
+                        "The bracket service's round does not only\
+                            have matches that are ready."
+                    )
+                matches.append(item)
+
+    # Map participant "objects" with IDs for easy lookup
+    participants = dict()
+    for item in tournament_data["included"]:
+        # Cleaner with match-case,
+        # and would also allow for just one iteration over tournament, not 2.
+        # Track in #549
+        if item["type"] == "participant":
+            id = item["id"]
+            participants[id] = item
+
+    match_objects = []
+    match_participant_objects = []
+
+    for m in matches:
+        match_object = apps.get_model("compete", "Match")(
+            episode=round.tournament.episode,
+            tournament_round=round,
+            alternate_order=True,
+            is_ranked=False,
+            challonge_id=m["id"],
+        )
+        match_objects.append(match_object)
+
+        # NOTE the following code is ridiculously inherent to challonge model.
+        # Should probably get participants in away that's cleaner
+        # tracked in #549
+        # NOTE could prob wrap this in a for loop for partipant 1 and 2
+        # tracked in #549
+        p1_id = m["relationships"]["player1"]["data"]["id"]
+        p1_misc_key = participants[p1_id]["attributes"]["misc"]
+        team_id_1, submission_id_1 = (int(_) for _ in p1_misc_key.split(","))
+        match_participant_1_object = apps.get_model("compete", "MatchParticipant")(
+            team_id=team_id_1,
+            submission_id=submission_id_1,
+            match=match_object,
+            # Note that player_index is 0-indexed.
+            # This may be tricky if you optimize code in #549.
+            player_index=0,
+            challonge_id=p1_id,
+        )
+        match_participant_objects.append(match_participant_1_object)
+
+        p2_id = m["relationships"]["player2"]["data"]["id"]
+        p2_misc_key = participants[p2_id]["attributes"]["misc"]
+        team_id_2, submission_id_2 = (int(_) for _ in p2_misc_key.split(","))
+        match_participant_2_object = apps.get_model("compete", "MatchParticipant")(
+            team_id=team_id_2,
+            submission_id=submission_id_2,
+            match=match_object,
+            # Note that player_index is 0-indexed.
+            # This may be tricky if you optimize code in #549.
+            player_index=1,
+            challonge_id=p2_id,
+        )
+        match_participant_objects.append(match_participant_2_object)
+
+    return match_objects, match_participant_objects
+
+
+def update_match(tournament_url, match_id, match):
     url = f"{URL_BASE}tournaments/{tournament_url}/matches/{match_id}.json"
 
     # Wrangle the Match object into a format Challonge likes.
--- backend/siarnaq/api/episodes/models.py
+++ backend/siarnaq/api/episodes/models.py
@@ -460,96 +460,12 @@ class TournamentRound(models.Model):
         if num_maps % 2 == 0:
             raise RuntimeError("The round does not have an odd number of maps.")
 
-        tournament = challonge.get_tournament(self.tournament.challonge_id_private)
-        # Derive matches of this round
-        # NOTE this probably makes more sense (efficiency and consistency)
-        # as a dict. Track in #549
-        matches = []
-        # Takes some wrangling with API response format
-        # We should move this block later
-        # (to keep all code that directly hits challonge
-        # in its own module) Track in #549
-        for item in tournament["included"]:
-            # Much cleaner w match-case and multiple keys.
-            # Track in #549
-            if item["type"] == "match":
-                round_idx = item["attributes"]["round"]
-                if round_idx == self.challonge_id:
-                    # Only enqueue the round if all matches are "open".
-                    # NOTE: it would be good to have a "force re-enqueue round",
-                    # which re-enqueues matches even if matches or round
-                    # already in progress.
-                    # This would change the following check --
-                    # matches could be open _or done_.
-                    # !!! This is also _really hard_ right now
-                    # cuz it involves match deletion which is really hard.
-                    # Track in #549
-                    if item["attributes"]["state"] != "open":
-                        # For later, have this raise a more specific exception.
-                        # Then have the caller handle this return
-                        # and translate it into an HTTP response.
-                        raise RuntimeError(
-                            "The bracket service's round does not only\
-                                have matches that are ready."
-                        )
-                    matches.append(item)
-
-        # Map participant "objects" with IDs for easy lookup
-        participants = dict()
-        for item in tournament["included"]:
-            # Cleaner with match-case,
-            # and would also allow for just one iteration over tournament, not 2.
-            # Track in #549
-            if item["type"] == "participant":
-                id = item["id"]
-                participants[id] = item
-
-        match_objects = []
-        maps_for_match_objects = []
-        match_participant_objects = []
-
-        for m in matches:
-            match_object = Match(
-                episode=self.tournament.episode,
-                tournament_round=self,
-                alternate_order=True,
-                is_ranked=False,
-                challonge_id=m["id"],
-            )
-            match_objects.append(match_object)
-
-            # NOTE the following code is ridiculously inherent to challonge model.
-            # Should probably get participants in away that's cleaner
-            # tracked in #549
-            # NOTE could prob wrap this in a for loop for partipant 1 and 2
-            # tracked in #549
-            p1_id = m["relationships"]["player1"]["data"]["id"]
-            p1_misc_key = participants[p1_id]["attributes"]["misc"]
-            team_id_1, submission_id_1 = (int(_) for _ in p1_misc_key.split(","))
-            match_participant_1_object = MatchParticipant(
-                team_id=team_id_1,
-                submission_id=submission_id_1,
-                match=match_object,
-                # Note that player_index is 0-indexed.
-                # This may be tricky if you optimize code in #549.
-                player_index=0,
-                challonge_id=p1_id,
-            )
-            match_participant_objects.append(match_participant_1_object)
-
-            p2_id = m["relationships"]["player2"]["data"]["id"]
-            p2_misc_key = participants[p2_id]["attributes"]["misc"]
-            team_id_2, submission_id_2 = (int(_) for _ in p2_misc_key.split(","))
-            match_participant_2_object = MatchParticipant(
-                team_id=team_id_2,
-                submission_id=submission_id_2,
-                match=match_object,
-                # Note that player_index is 0-indexed.
-                # This may be tricky if you optimize code in #549.
-                player_index=1,
-                challonge_id=p2_id,
-            )
-            match_participant_objects.append(match_participant_2_object)
+        (
+            match_objects,
+            match_participant_objects,
+        ) = challonge.get_match_and_participant_objects_for_round(
+            self.tournament.challonge_id_private, self
+        )
 
         with transaction.atomic():
             matches = Match.objects.bulk_create(match_objects)
