b3f69493ae1b86c5e8af93b4c1cbc4cad0947500
Jerry Mao <j-mao@users.noreply.github.com>
2023-01-19 15:05:39 -0500

    Reduce load ratio for execution

--- deploy/galaxy/main.tf
+++ deploy/galaxy/main.tf
@@ -223,7 +223,7 @@ module "saturn_execute" {
 
   max_instances = var.max_execute_instances
   min_instances = var.min_execute_instances
-  load_ratio    = 5
+  load_ratio    = 2
 }
 
 resource "google_cloudbuild_trigger" "saturn" {
