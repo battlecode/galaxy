3ae2ff3cd36153f78f4a9399b8e2db9e06a7256c
Lowell Torola <lmtorola@mit.edu>
2023-06-20 17:17:33 -0400

    updated gen script

--- frontend2/generate_types.bat
+++ frontend2/generate_types.bat
@@ -4,5 +4,8 @@ echo "navigate to galaxy folder"
 cd ..
 echo "Generate OpenAPI 3.0 backend schema from siarnaq"
 python backend/manage.py spectacular --file schema.yml
+:: echo "Download openapitools"
+:: npm install @openapitools/openapi-generator-cli -g
+:: if you have a java error, just install java 8 and reload terminal
 echo "Generate typescript types from schema"
 npx @openapitools/openapi-generator-cli generate -i schema.yml -o frontend2/utils/types -g typescript-jquery
--- frontend2/utils/types/api/api.ts
+++ frontend2/utils/types/api/api.ts
@@ -1,3 +1,5 @@
 export * from './ApiApi';
 import { ApiApi } from './ApiApi';
 export const APIS = [ApiApi];
+
+// make changes here... not sure what to add...
