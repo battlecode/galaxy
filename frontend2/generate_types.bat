2a36c6e2cba4e41b8ed796f9a6e4377ec285da2f
Lowell Torola <44183219+lowtorola@users.noreply.github.com>
2023-06-27 21:14:58 -0400

    Api/Auth (#652)
    Yayy API!!

---
+++ frontend2/generate_types.bat
@@ -0,0 +1,11 @@
+echo "Running generate_types.bat"
+call conda activate galaxy
+echo "navigate to galaxy folder"
+cd ..
+echo "Generate OpenAPI 3.0 backend schema from siarnaq"
+python backend/manage.py spectacular --file schema.yml
+:: echo "Download openapitools"
+:: npm install @openapitools/openapi-generator-cli -g
+:: if you have a java error, just install java 8 and reload terminal
+echo "Generate typescript types from schema"
+npx @openapitools/openapi-generator-cli generate -i schema.yml -o frontend2/src/utils/types -g typescript-jquery --additional-properties=modelPropertyNaming=camelCase --additional-properties=disallowAdditionalPropertiesIfNotPresent=false
