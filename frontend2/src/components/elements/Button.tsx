93aedd0de89423f828cbb020c13296fe113df5f3
ehemphill <84475123+apollo1291@users.noreply.github.com>
2023-08-29 16:58:35 -0400

    Header and minor ui changes (#654)
    Co-authored-by: Serena Li <serena.li.usa@gmail.com>

--- frontend2/src/components/elements/Button.tsx
+++ frontend2/src/components/elements/Button.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Icon, { type IconName } from "./Icon";
 
+const VARIANTS: Record<string, string> = {
+  "": "bg-gray-50 text-gray-800 hover:bg-gray-100 hover:ring-gray-900 hover:text-black ring-gray-500 ring-1 ring-inset",
+  dark: "bg-cyan-700 text-white hover:bg-cyan-800",
+  "light-outline":
+    "ring-2 ring-inset ring-gray-200 text-gray-200 hover:bg-gray-100/20",
+} as const;
+
 interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
   variant?: string;
   label?: string;
@@ -8,11 +15,6 @@ interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
   fullWidth?: boolean;
   className?: string;
 }
-
-const variants: Record<string, string> = {
-  "": "bg-gray-50 text-gray-800 hover:bg-gray-100 hover:ring-gray-900 hover:text-black ring-gray-500 ring-1 ring-inset",
-  dark: "bg-cyan-700 text-white hover:bg-cyan-800",
-};
 
 const Button: React.FC<ButtonProps> = ({
   variant = "",
@@ -22,14 +24,15 @@ const Button: React.FC<ButtonProps> = ({
   className = "",
   ...rest
 }) => {
-  const variantStyle = `${variants[variant]}  ${
+  const variantStyle = `${VARIANTS[variant]}  ${
     fullWidth ? "w-full" : ""
   } ${className}`;
   return (
     <button
       // default button type
       type="button"
-      className={`flex h-9 flex-row items-center justify-center gap-1.5 rounded px-2.5 py-1.5 font-medium shadow-sm ${variantStyle}`}
+      className={`flex h-9 flex-row items-center justify-center gap-1.5 rounded px-2.5
+        py-1.5 shadow-sm ${variantStyle} ${className}`}
       {...rest}
     >
       {iconName !== undefined && <Icon name={iconName} size="sm" />}
