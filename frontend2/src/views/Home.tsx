6f6c47447e51de316cca094df0a30d978c9e0ecf
Serena Li <serena.li.usa@gmail.com>
2023-08-31 12:10:06 -0700

    undo home changes

--- frontend2/src/views/Home.tsx
+++ frontend2/src/views/Home.tsx
@@ -1,37 +1,7 @@
 import React from "react";
-import Icon from "../components/elements/Icon";
-import Collapse from "../components/elements/Collapse";
 
 const Home: React.FC = () => {
-  return (
-    <div className="flex flex-col gap-6 bg-white w-full p-20">
-      This is a page that contains collapsible content:
-      <Collapse title="This is the title">
-        <p>Some content here</p>
-        <div className="flex flex-row gap-2">
-          <Icon name="check" />
-          <p>alskdjfasdf</p>
-          <h1>asdfljkasldkfj</h1>
-        </div>
-      </Collapse>
-      <Collapse title="This is the title">
-        <p>Some content here</p>
-        <div className="flex flex-row gap-2">
-          <Icon name="check" />
-          <p>alskdjfasdf</p>
-          <h1>asdfljkasldkfj</h1>
-        </div>
-      </Collapse>
-      <Collapse title="This is the title">
-        <p>Some content here</p>
-        <div className="flex flex-row gap-2">
-          <Icon name="check" />
-          <p>alskdjfasdf</p>
-          <h1>asdfljkasldkfj</h1>
-        </div>
-      </Collapse>
-    </div>
-  );
+  return <p>Homepage</p>;
 };
 
 export default Home;
