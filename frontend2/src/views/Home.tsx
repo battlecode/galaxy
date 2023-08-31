c9b6e986f058c73aae8c462a7549ff203cd31e34
Serena Li <serena.li.usa@gmail.com>
2023-08-31 11:55:38 -0700

    Add collapsible component

--- frontend2/src/views/Home.tsx
+++ frontend2/src/views/Home.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Icon from "../components/elements/Icon";
+import Collapse from "../components/elements/Collapse";
 
 const Home: React.FC = () => {
-  return <p>Homepage</p>;
+  return (
+    <div className="flex flex-col gap-6 bg-white w-full p-20">
+      This is a page that contains collapsible content:
+      <Collapse title="This is the title">
+        <p>Some content here</p>
+        <div className="flex flex-row gap-2">
+          <Icon name="check" />
+          <p>alskdjfasdf</p>
+          <h1>asdfljkasldkfj</h1>
+        </div>
+      </Collapse>
+      <Collapse title="This is the title">
+        <p>Some content here</p>
+        <div className="flex flex-row gap-2">
+          <Icon name="check" />
+          <p>alskdjfasdf</p>
+          <h1>asdfljkasldkfj</h1>
+        </div>
+      </Collapse>
+      <Collapse title="This is the title">
+        <p>Some content here</p>
+        <div className="flex flex-row gap-2">
+          <Icon name="check" />
+          <p>alskdjfasdf</p>
+          <h1>asdfljkasldkfj</h1>
+        </div>
+      </Collapse>
+    </div>
+  );
 };
 
 export default Home;
