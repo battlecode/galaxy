c0b428f7ed1b104a6b21449d3b7f977b8996d49b
Serena Li <serena.li.usa@gmail.com>
2023-09-30 13:20:11 -0400

    update test

--- frontend2/src/components/sidebar/__test__/Sidebar.test.tsx
+++ frontend2/src/components/sidebar/__test__/Sidebar.test.tsx
@@ -1,29 +1,17 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Sidebar from "../";
-import { DEFAULT_EPISODE } from "../../../utils/constants";
 import { EpisodeIdContext } from "../../../contexts/EpisodeContext";
 import { MemoryRouter } from "react-router-dom";
-
-test("UI: should link to default episode", () => {
-  render(
-    <MemoryRouter>
-      <Sidebar />
-    </MemoryRouter>,
-  );
-  const linkElement = screen
-    .getByText("Resources")
-    .closest("a")
-    ?.getAttribute("href");
-  expect(linkElement).toEqual(
-    expect.stringContaining(`/${DEFAULT_EPISODE}/resources`),
-  );
-});
 
 test("UI: should collapse sidebar", () => {
   render(
     <MemoryRouter>
-      <Sidebar collapsed={true} />
+      <EpisodeIdContext.Provider
+        value={{ episodeId: "something", setEpisodeId: (_) => undefined }}
+      >
+        <Sidebar collapsed={true} />
+      </EpisodeIdContext.Provider>
     </MemoryRouter>,
   );
   expect(screen.queryByText("Home")).toBeNull();
