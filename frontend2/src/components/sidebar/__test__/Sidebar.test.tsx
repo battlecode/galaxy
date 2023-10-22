5ccc2a2004ff8f0e05b10f4ffdf40a3c666da51e
Anusri Saraf <anusrisaraf20@gmail.com>
2023-10-21 22:35:11 -0400

    Add team sidebar display conditions (#689)

--- frontend2/src/components/sidebar/__test__/Sidebar.test.tsx
+++ frontend2/src/components/sidebar/__test__/Sidebar.test.tsx
@@ -2,7 +2,35 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Sidebar from "../";
 import { EpisodeIdContext } from "../../../contexts/EpisodeContext";
+import { CurrentUserContext } from "../../../contexts/CurrentUserContext";
+import {
+  CurrentTeamContext,
+  TeamStateEnum,
+} from "../../../contexts/CurrentTeamContext";
 import { MemoryRouter } from "react-router-dom";
+import { Status526Enum } from "../../../utils/types";
+
+const mem = {
+  id: 123,
+  username: "theuser",
+  is_staff: false,
+};
+const faketeam = {
+  id: 123,
+  episode: "bc23",
+  name: "theteam",
+  members: [mem],
+  join_key: "abc",
+  status: Status526Enum.O,
+};
+const fakeuser = {
+  id: 123,
+  username: "theuser",
+  email: "user@gmail.com",
+  first_name: "the",
+  last_name: "user",
+  is_staff: false,
+};
 
 test("UI: should collapse sidebar", () => {
   render(
@@ -10,7 +38,24 @@ test("UI: should collapse sidebar", () => {
       <EpisodeIdContext.Provider
         value={{ episodeId: "something", setEpisodeId: (_) => undefined }}
       >
-        <Sidebar collapsed={true} />
+        <CurrentTeamContext.Provider
+          value={{ teamState: TeamStateEnum.IN_TEAM, team: faketeam }}
+        >
+          <CurrentUserContext.Provider
+            value={{
+              authState: "loading",
+              user: fakeuser,
+              login: (_) => {
+                console.log();
+              },
+              logout: () => {
+                console.log();
+              },
+            }}
+          >
+            <Sidebar collapsed={true} />
+          </CurrentUserContext.Provider>
+        </CurrentTeamContext.Provider>
       </EpisodeIdContext.Provider>
     </MemoryRouter>,
   );
@@ -23,7 +68,24 @@ test("UI: should link to episode in surrounding context", () => {
       <EpisodeIdContext.Provider
         value={{ episodeId: "something", setEpisodeId: (_) => undefined }}
       >
-        <Sidebar />
+        <CurrentTeamContext.Provider
+          value={{ teamState: TeamStateEnum.IN_TEAM, team: faketeam }}
+        >
+          <CurrentUserContext.Provider
+            value={{
+              authState: "loading",
+              user: fakeuser,
+              login: (_) => {
+                console.log();
+              },
+              logout: () => {
+                console.log();
+              },
+            }}
+          >
+            <Sidebar />
+          </CurrentUserContext.Provider>
+        </CurrentTeamContext.Provider>
       </EpisodeIdContext.Provider>
     </MemoryRouter>,
   );
