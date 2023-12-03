63839caeddbd608d746667e27b7c41b4def9ea71
Lowell <lmtorola@mit.edu>
2023-12-03 14:47:15 -0500

    rip out replaced contexts

--- frontend2/src/contexts/EpisodeContext.ts
+++ frontend2/src/contexts/EpisodeContext.ts
@@ -1,24 +1,13 @@
 import { createContext, useContext } from "react";
-import { type Episode } from "../utils/types";
-import { type Maybe } from "../utils/utilTypes";
 
 interface EpisodeIdContextType {
   episodeId: string;
   setEpisodeId: (episodeId: string) => void;
 }
 
-// export const EpisodeContext = createContext<Maybe<Episode>>(undefined);
 export const EpisodeIdContext = createContext<EpisodeIdContextType | null>(
   null,
 );
-// export const EpisodeListContext = createContext<Maybe<Episode[]>>(undefined);
-
-// Use this function to retrieve full episode information. If the api call to
-// retrieve full episode information has not completed, then
-// episodeContext.episode will be undefined.
-// export const useEpisode = (): Maybe<Episode> => {
-//   return useContext(EpisodeContext);
-// };
 
 // Use this function to retrieve and update the episodeId.
 export const useEpisodeId = (): EpisodeIdContextType => {
@@ -28,8 +17,3 @@ export const useEpisodeId = (): EpisodeIdContextType => {
   }
   return episodeIdContext;
 };
-
-/** Use this function to retrieve all episodes. */
-// export const useEpisodeList = (): Maybe<Episode[]> => {
-//   return useContext(EpisodeListContext);
-// };
