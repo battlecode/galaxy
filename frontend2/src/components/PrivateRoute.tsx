15ae0ef7e18925687d9bb38f579909d7a9c4f8a6
Serena Li <serena.li.usa@gmail.com>
2024-01-30 13:04:42 -0600

    fix routing on login page?

--- frontend2/src/components/PrivateRoute.tsx
+++ frontend2/src/components/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AuthStateEnum, useCurrentUser } from "../contexts/CurrentUserContext";
 import { Outlet, useNavigate } from "react-router-dom";
 import Spinner from "./Spinner";
@@ -6,11 +6,15 @@ import Spinner from "./Spinner";
 const PrivateRoute: React.FC = () => {
   const { authState } = useCurrentUser();
   const navigate = useNavigate();
+  useEffect(() => {
+    // redirect to home if already logged in
+    if (authState === AuthStateEnum.NOT_AUTHENTICATED) {
+      navigate("/login");
+    }
+  }, [authState]);
+
   if (authState === AuthStateEnum.AUTHENTICATED) {
     return <Outlet />;
-  } else if (authState === AuthStateEnum.NOT_AUTHENTICATED) {
-    navigate("/login");
-    return null;
   } else {
     return (
       <div className="flex h-screen items-center justify-center">
