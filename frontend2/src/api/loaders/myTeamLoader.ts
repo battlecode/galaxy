18a7061965e51afb33ca9e5384c78c660562d92d
Lowell <lmtorola@mit.edu>
2024-03-10 00:55:12 -0500

    lint fix

--- frontend2/src/api/loaders/myTeamLoader.ts
+++ frontend2/src/api/loaders/myTeamLoader.ts
@@ -1,5 +1,5 @@
-import { QueryClient } from "@tanstack/react-query";
-import { LoaderFunction } from "react-router-dom";
+import type { QueryClient } from "@tanstack/react-query";
+import type { LoaderFunction } from "react-router-dom";
 
 export const myTeamLoader =
   (queryClient: QueryClient): LoaderFunction =>
@@ -7,6 +7,8 @@ export const myTeamLoader =
     const episodeId = params.episodeId;
 
     if (episodeId === undefined) return null;
+
+    // needs: useUserTeam
 
     return null;
   };
