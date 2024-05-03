d919d8cd9e03f5738cec01ae008def458f569007
Nour Massri <mnmassri@mit.edu>
2024-05-03 14:49:58 -0400

    precommit trim trailing whitespaces

--- frontend2/src/content/bc24.ts
+++ frontend2/src/content/bc24.ts
@@ -345,9 +345,9 @@ export const BC24_TOURNAMENTS: Record<TourneyPageKey, string> = {
   - **Final Tournament prizes:** Prizes will range from $5000 for 1st place to $500 for 16th place.
   - **Sprint prizes:** Winner of each tournament will receive $250.
   - **Newbie, High School prizes:** Prizes will range from $600 to $200 for the top 3 teams.
-  
+
   If you are an international participant, please note that [US export regulations](https://www.bis.doc.gov/index.php/policy-guidance/country-guidance/sanctioned-destinations) may restrict our ability to award prizes to students from certain countries.
-  
+
   `,
   format: `# Tournament Format
 
@@ -363,20 +363,20 @@ export const BC24_TOURNAMENTS: Record<TourneyPageKey, string> = {
   Anyone can write a bot, create a team, and participate in scrimmage matches/rankings. The Sprint Tournaments are open to everyone, but the other tournaments have stricter eligibility rules.
 
   Your team must meet **all three conditions** by a tournament's submission deadline to be eligible for it:
-  
+
   1. Have uploaded a bot
   2. Have correctly indicated your eligibility on your Team Profile page
   3. Have all members upload a resume, at your personal profile page (This condition does not apply to the Sprint Tournaments).
-  
+
   Additionally, tournament specific eligibility is listed below:
-  
+
   - **Sprint Tournament:** All teams are eligible.
   - **US Qualifier:** Teams must **consist entirely of US college students** studying full-time, or in a transition phase.
   - **International Qualifier:** Teams must **consist entirely of college students** studying full-time, or in a transition phase, where at least one team member is not a US student.
   - **MIT Newbie Tournament:** Teams must **consist entirely of MIT students** who have never competed in Battlecode before.
   - **High School Tournament:** Teams must **consist entirely of high school students**.
   - **Final Tournament:** Teams must have qualified via the US or International Qualifier. The final match of the Newbie and High School tournaments will also be played at the final tournament.
-  
+
   Contact us on [Discord](https://discord.gg/N86mxkH) or at [battlecode@mit.edu](mailto:battlecode@mit.edu) if you are unsure of your eligibility.
   `,
 };
