7990a01ad82aa3ad1e033d3b897583198a11979c
Nour Massri <mnmassri@mit.edu>
2024-10-04 14:16:20 -0400

    DRY in contentStruct.ts

--- frontend2/src/content/ContentStruct.ts
+++ frontend2/src/content/ContentStruct.ts
@@ -1,9 +1,3 @@
-export type QuickStartPageKey =
-  | "overview"
-  | "creation"
-  | "installation"
-  | "resources"
-  | "community";
 export enum QuickStartPage {
   OVERVIEW = "overview",
   ACCOUNT_AND_TEAM_CREATION = "creation",
@@ -11,47 +5,36 @@ export enum QuickStartPage {
   RESOURCES = "resources",
   JOIN_THE_COMMUNITY = "community",
 }
+export type QuickStartPageKey = `${QuickStartPage}`;
 
-export type ResourcesPageKey =
-  | "specification"
-  | "resources"
-  | "tools"
-  | "lectures";
 export enum ResourcesPage {
   GAME_SPECIFICATION = "specification",
   CODING_RESOURCES = "resources",
   THIRD_PARTY_TOOLS = "tools",
   LECTURES = "lectures",
 }
+export type ResourcesPageKey = `${ResourcesPage}`
 
-export type CommonIssuesPageKey =
-  | "installation"
-  | "client"
-  | "other"
-  | "things";
 export enum CommonIssuesPage {
   INSTALLATION_ISSUES = "installation",
   CLIENT_ISSUES = "client",
   OTHER_ISSUES = "other",
   THINGS_TO_TRY = "things",
 }
+export type CommonIssuesPageKey = `${CommonIssuesPage}`
 
-export type DebuggingTipsPageKey =
-  | "debugging"
-  | "intellij"
-  | "eclipse"
-  | "second_method";
 export enum DebuggingTipsPage {
   DEBUGGING = "debugging",
   INTELLIJ = "intellij",
   ECLIPSE = "eclipse",
   SECOND_METHOD = "second_method",
 }
+export type DebuggingTipsPageKey = `${DebuggingTipsPage}`
 
-export type TourneyPageKey = "schedule" | "prizes" | "format" | "rules";
 export enum TourneyPage {
   SCHEDULE = "schedule",
   PRIZES = "prizes",
   FORMAT = "format",
   RULES = "rules",
 }
+export type TourneyPageKey = `${TourneyPage}`
