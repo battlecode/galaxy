6fd38cc94e8605b9ab6f190bc46b16ba8e9f95b0
Leo <47073066+jaranmio@users.noreply.github.com>
2024-11-14 21:35:13 -0500

    Edit Not Found Page (#841)
    Co-authored-by: Ngueho Kana Léo Keran <leo@K902721P6M.tld>

--- frontend2/src/views/PageNotFound.tsx
+++ frontend2/src/views/PageNotFound.tsx
@@ -1,16 +1,39 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { DEFAULT_EPISODE } from "utils/constants";
+import { useNavigate } from "react-router-dom";
+import Button from "components/elements/Button";
+import { useEpisodeId } from "contexts/EpisodeContext";
 
 const PageNotFound: React.FC = () => {
+  const navigate = useNavigate();
+  const { episodeId } = useEpisodeId();
   return (
-    <div className="flex h-full w-full flex-col overflow-auto p-6">
-      <p>
-        That page was not found.{" "}
-        <NavLink to={`/${DEFAULT_EPISODE}/home`} className="hover:underline">
-          Go Home?
-        </NavLink>
-      </p>
+    <div className="flex h-full w-full flex-col items-center justify-center gap-8 overflow-auto">
+      <div className="flex h-fit w-full flex-row justify-center overflow-auto">
+        <img
+          className="bg-grey h-64 w-64 rounded-full fill-cyan-500"
+          src="/number_4.svg"
+        />
+        <img
+          className="bg-grey h-64 w-64 rounded-full"
+          src="/battlecode-logo-vert-white.png"
+        />
+        <img
+          className="bg-grey h-64 w-64 rounded-full fill-cyan-500"
+          src="/number_4.svg"
+        />
+      </div>
+
+      <span className="font-large text-5xl font-bold text-gray-700">
+        Page Not Found
+      </span>
+
+      <Button
+        variant="dark"
+        label="Go Home?"
+        onClick={() => {
+          navigate(`/${episodeId}/home`);
+        }}
+      />
     </div>
   );
 };
