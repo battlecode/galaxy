ff1dd3bb9931f6301b609a164dba75ee4e799fe6
Lowell Torola <lmtorola@mit.edu>
2024-12-06 17:26:52 -0500

    button and rebase

--- frontend2/src/views/ErrorBoundary.tsx
+++ frontend2/src/views/ErrorBoundary.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { useRouteError, Link } from "react-router-dom";
+import type React from "react";
+import { useRouteError, useNavigate } from "react-router-dom";
 import { PageTitle } from "../components/elements/BattlecodeStyle";
 import { useEpisodeId } from "../contexts/EpisodeContext";
+import Button from "components/elements/Button";
 
 const ErrorBoundary: React.FC = () => {
   const error = useRouteError();
   const { episodeId } = useEpisodeId();
+  const navigate = useNavigate();
 
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-white p-6">
+    <div className="flex h-full items-center justify-center bg-white p-6">
       <div className="flex w-full max-w-xs flex-col gap-4 rounded-lg border border-gray-300 bg-gray-50 p-6 shadow-md">
         <PageTitle>Oops, something went wrong.</PageTitle>
         <p className="text-center">
@@ -24,11 +26,13 @@ const ErrorBoundary: React.FC = () => {
             An unknown error occurred.
           </span>
         )}
-        <Link to={episodeId !== undefined ? `/${episodeId}/home` : "/"}>
-          <div className="mx-auto mt-4 rounded bg-blue-500 px-3 py-1 text-center text-white transition hover:bg-blue-600">
-            Go to Home
-          </div>
-        </Link>
+        <Button
+          onClick={() => {
+            navigate(`/${episodeId}/home`);
+          }}
+          label="Go to Home"
+          variant="dark"
+        />
       </div>
     </div>
   );
