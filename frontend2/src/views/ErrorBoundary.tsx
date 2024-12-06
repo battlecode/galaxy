813cce6688feebfd55d713b7087e489cb12052da
beshrislambouli <155996919+beshrislambouli@users.noreply.github.com>
2024-12-06 17:29:26 -0500

    Prettier error-page (#836)
    Co-authored-by: Beshr Islam Bouli <beshr@VN9RNYQM3Y.tld>
    Co-authored-by: Lowell Torola <lmtorola@mit.edu>

--- frontend2/src/views/ErrorBoundary.tsx
+++ frontend2/src/views/ErrorBoundary.tsx
@@ -1,26 +1,41 @@
 import type React from "react";
-import { useRouteError } from "react-router-dom";
+import { useRouteError, useNavigate } from "react-router-dom";
 import { PageTitle } from "../components/elements/BattlecodeStyle";
+import { useEpisodeId } from "../contexts/EpisodeContext";
+import Button from "components/elements/Button";
 
 const ErrorBoundary: React.FC = () => {
   const error = useRouteError();
+  const { episodeId } = useEpisodeId();
+  const navigate = useNavigate();
 
-  if (error instanceof Error) {
-    return (
-      <div className="flex flex-col gap-6 p-6 xl:flex-row">
+  return (
+    <div className="flex h-full items-center justify-center bg-white p-6">
+      <div className="flex w-full max-w-xs flex-col gap-4 rounded-lg border border-gray-300 bg-gray-50 p-6 shadow-md">
         <PageTitle>Oops, something went wrong.</PageTitle>
-        <p>
+        <p className="text-center">
           Please reach out on Discord or to battlecode@mit.edu with the
           following error message:
         </p>
-        <span className="rounded-md bg-gray-200 px-1 py-0.5 font-mono">
-          {error.message}
-        </span>
+        {error instanceof Error ? (
+          <span className="block rounded-md bg-gray-200 px-1 py-0.5 text-center font-mono">
+            {error.message}
+          </span>
+        ) : (
+          <span className="block text-center text-red-600">
+            An unknown error occurred.
+          </span>
+        )}
+        <Button
+          onClick={() => {
+            navigate(`/${episodeId}/home`);
+          }}
+          label="Go to Home"
+          variant="dark"
+        />
       </div>
-    );
-  }
-
-  return <p>Oops, something went wrong.</p>;
+    </div>
+  );
 };
 
 export default ErrorBoundary;
