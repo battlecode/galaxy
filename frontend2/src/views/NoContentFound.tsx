b3b96d70d136df6b3c633407eed1c375ba0f7fc5
Serena Li <serena.li.usa@gmail.com>
2024-12-06 11:49:27 -0500

    change not found message to include email add

--- frontend2/src/views/NoContentFound.tsx
+++ frontend2/src/views/NoContentFound.tsx
@@ -3,9 +3,9 @@ import type React from "react";
 const NoContentFound: React.FC = () => (
   <div className="flex h-full w-full flex-col overflow-auto p-6">
     <p>
-      {`No content found for this episode. If you have any questions, please `}
-      <a href="mailto:battlecode.mit.edu" className="hover:underline">
-        {`reach out to Battlecode staff`}
+      {`No content found for this episode. If you have any questions, please reach out to Battlecode staff at `}
+      <a href="mailto:battlecode@mit.edu" className="hover:underline">
+        {`battlecode@mit.edu`}
       </a>
       {". "}
     </p>
