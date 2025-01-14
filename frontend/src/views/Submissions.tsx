a8e45f7848d86a9dfdd5894bbdd227c3f4185f34
Lowell Torola <lmtorola@mit.edu>
2025-01-14 08:48:46 -0500

    manual autoscrim and allow staff submissions

--- frontend/src/views/Submissions.tsx
+++ frontend/src/views/Submissions.tsx
@@ -21,6 +21,7 @@ import { getParamEntries, parsePageParam } from "../utils/searchParamHelpers";
 import { useUserTeam } from "api/team/useTeam";
 import { Status526Enum, LanguageEnum } from "api/_autogen";
 import { PageTitle } from "components/elements/BattlecodeStyle";
+import { useCurrentUserInfo } from "api/user/useUser";
 
 interface SubmissionFormInput {
   file: FileList;
@@ -109,9 +110,9 @@ const Submissions: React.FC = () => {
 
 const CodeSubmission: React.FC = () => {
   const { episodeId } = useEpisodeId();
-  const teamData = useUserTeam({ episodeId });
   const queryClient = useQueryClient();
 
+  const userData = useCurrentUserInfo();
   const episode = useEpisodeInfo({ id: episodeId });
 
   const uploadSub = useUploadSubmission(
@@ -141,8 +142,8 @@ const CodeSubmission: React.FC = () => {
 
   if (!episode.isSuccess) return null;
 
-  const isStaffTeam = teamData.data?.status === Status526Enum.S;
-  if (!isStaffTeam && episode.data.frozen)
+  const userIsStaff = userData.isSuccess && userData.data.is_staff;
+  if (!userIsStaff && episode.data.frozen)
     return (
       <p>
         Submissions are currently frozen! This is most likely due to a
