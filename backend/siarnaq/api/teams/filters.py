5235345223d84dc943988eea40d0a251b41b4661
Lowell Torola <44183219+lowtorola@users.noreply.github.com>
2025-01-15 00:45:31 +0400

    Rankings filter by eligibility ANDed (#911)
    Co-authored-by: Nour Massri <mnmassri@mit.edu>

--- backend/siarnaq/api/teams/filters.py
+++ backend/siarnaq/api/teams/filters.py
@@ -1,4 +1,8 @@
+import structlog
+from django.db.models import Count
 from rest_framework import filters
+
+logger = structlog.get_logger(__name__)
 
 
 class TeamOrderingFilter(filters.OrderingFilter):
@@ -37,6 +41,19 @@ class TeamEligibilityFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         if "eligible_for" not in request.query_params:
             return queryset
-        return queryset.filter(
-            profile__eligible_for__in=request.query_params.getlist("eligible_for")
+
+        eligible_for_list = request.query_params.getlist("eligible_for")
+
+        # Filter teams that have all the specified eligible_for values
+        # tricky query, this results in a join becasue eligible_for is a many to many
+        filtered_teams = (
+            queryset.filter(profile__eligible_for__in=eligible_for_list)
+            .annotate(eligible_count=Count("profile__eligible_for"))
+            .filter(eligible_count=len(eligible_for_list))
         )
+        logger.info(
+            "TeamEligibilityFilter",
+            message="TeamEligibilityFilter Query Plan:",
+            queryPlan=str(filtered_teams.query),
+        )
+        return filtered_teams
