b7646fb45d8ae033f582a524eb75cdf56534b398
Nour Massri <mnmassri@mit.edu>
2026-01-03 08:46:18 -0500

    Running saturn locally (#920)

--- saturn/pkg/saturn/secret.go
+++ saturn/pkg/saturn/secret.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
@@ -13,7 +14,21 @@ type Secret struct {
 	GitToken string `json:"git-token"`
 }
 
-func ReadSecret(ctx context.Context, projectID, name string) (*Secret, error) {
+func ReadSecret(ctx context.Context, projectID, name string, onSaturn bool) (*Secret, error) {
+	if !onSaturn {
+		// Read local secret file and parse as *Secret
+		filePath := name
+		content, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			return nil, fmt.Errorf("ioutil.ReadFile: %v", err)
+		}
+
+		var result Secret
+		if err := json.Unmarshal(content, &result); err != nil {
+			return nil, fmt.Errorf("json.Unmarshal: %v", err)
+		}
+		return &result, nil
+	}
 	client, err := secretmanager.NewClient(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("secretmanager.NewClient: %v", err)
